22cfcd16c46dc973a848df9d78d49881926b5037
e <zerlzerl@163.com>
2019-09-22 13:27:44 +0800

    new feature model

---
+++ model_converter.py
@@ -0,0 +1,24 @@
+import time
+
+import tensorflow as tf
+from tensorflow.python import keras
+
+h5_model_path = "./model/0922/weights-improvement-02-120.88.h5"
+tf.keras.backend.set_learning_phase(0)
+model = keras.models.load_model(h5_model_path)
+export_path = './model/tf/' + str(int(time.time()))
+print(model.inputs)
+print(model.outputs)
+#
+with tf.keras.backend.get_session() as sess:
+    # model_input = tf.placeholder(tf.float32, [None, 44])
+    # model_output = tf.placeholder(tf.float32, [None])
+    model_input = tf.placeholder(tf.float32, [None, 51])
+    model_output = model(model_input)
+    tf.saved_model.simple_save(
+        sess,
+        export_path,
+        inputs={'myInput': model_input},
+        outputs={'myOutput': model_output})
+
+
